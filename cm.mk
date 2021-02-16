921421aaebf31aa7e185dba9104daaa460bcab7d
Amy <54113654+00p513-dev@users.noreply.github.com>
2021-02-16 15:24:43 +0000

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 # Release name
-PRODUCT_RELEASE_NAME := A311
+PRODUCT_RELEASE_NAME := bidder
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/Micromax/A311/device_A311.mk)
+$(call inherit-product, device/estar/bidder/device_bidder.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := A311
-PRODUCT_NAME := cm_A311
-PRODUCT_BRAND := Micromax
-PRODUCT_MODEL := A311
-PRODUCT_MANUFACTURER := Micromax
+PRODUCT_DEVICE := bidder
+PRODUCT_NAME := cm_bidder
+PRODUCT_BRAND := EStar
+PRODUCT_MODEL := bidder
+PRODUCT_MANUFACTURER := EStar
 
 ##PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="mt6582-user 6.0 MRA58M 2280749 release-keys"
